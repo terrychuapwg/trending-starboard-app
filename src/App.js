bda55359ecae545151233eecc01fb1765b4e45fd
terrychuapwg <terrychuapwg@gmail.com>
2025-07-25 17:36:36 +0800

    refactor: Modify App.js to implements router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import TrendingPage from '@pages/TrendingPage/TrendingPage.jsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ViewRepo from '@pages/TrendingPage/ViewRepo/ViewRepo.jsx';
+
 
 function App() {
   return (
-    <div className="App">
-      <p className='text-2xl text-teal-500'>Test tailwindcss</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/" element={<TrendingPage />} />
+        <Route path="/trending" element={<TrendingPage />} />
+        <Route path="/trending/view/:id" element={<ViewRepo />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
