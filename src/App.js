59864aaf0c71e4c7c2911169647154954965e5b1
terrychuapwg <terrychuapwg@gmail.com>
2025-07-25 11:50:47 +0800

    chore: initial project setup with React 18, TailwindCSS, Ant Design, CRACO, AOS, React Router, and TanStack

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p className='text-2xl text-teal-500'>Test tailwindcss</p>
+    </div>
+  );
+}
+
+export default App;
